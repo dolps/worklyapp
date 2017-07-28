02f310b18f31477d16b0ac9e0aa2fbd273a09ab8
Thomas Dølplads <thomas.dolplads@avanade.com>
2017-07-28 14:17:50 +0200

    created workweek page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+//import { HomePage } from '../home/home';
+import {WorkweekPage } from '../workweek/workweek'
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
+  //tab1Root = HomePage;
+  tab1Root = WorkweekPage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
---
+++ src/pages/workweek/workweek.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { WorkweekPage } from './workweek';
+
+@NgModule({
+  declarations: [
+    WorkweekPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(WorkweekPage),
+  ],
+})
+export class WorkweekPageModule {}
